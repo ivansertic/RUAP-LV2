361f85e9900e9bf47ae41ebf1a5099de110b5aaf
ivansertic <serticivan5@gmail.com>
2019-11-07 17:59:48 +0100

    Exercise 1: Create a Read-Only Web API

---
+++ Begin/ContactManager/Services/ContactRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using ContactManager.Models;
+
+namespace ContactManager.Services
+{
+    public class ContactRepository
+    {
+        public Contact[] GetAllContacts()
+        {
+            return new Contact[]
+            {
+                new Contact
+                {
+                    id = 1,
+                    Name = "Glenn Block"
+                },
+                new Contact
+                {
+                    id = 2,
+                    Name = "Dan Roth"
+                }
+            };
+        }
+    }
+}
